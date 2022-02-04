ff627213ce678cba6ab4d87c574bd5539e8ae903
gregorio <gregorio.gerardi@reflektion.com>
2022-02-04 20:59:21 -0300

    Added carrousel

--- src/Content/Gallery/index.tsx
+++ src/Content/Gallery/index.tsx
@@ -1,30 +1,54 @@
 import Section from "../../Utils/Wrappers/Section";
 import style from './style.module.css'
-import fonts from 'theme/fonts/fonts.module.css'
-import classNames from "classnames";
-import perrito from "assets/dogs/dog1.jpg";
+import pastorAustraliano from "assets/dogs/pastorAustraliano.jpeg";
 import {useHashScroll} from "react-hash-scroll";
-import {useHashScrollDefaultOptions} from "../../Layout/constant";
+import {useHashScrollDefaultOptions} from "Layout/constant";
+import AliceCarousel from 'react-alice-carousel';
+import {SyntheticEvent} from "react";
 
-const nftContentCls = classNames(style.nftContent, fonts.defaultFont)
-const contentCls = classNames(style.content)
-const nftDescriptionCls = classNames(style.nftDescription)
+const handleDragStart = (e: SyntheticEvent) => e.preventDefault();
+
+const responsive = {
+    0: {items: 1},
+    512: {items: 2},
+    768: {items: 3},
+    1024: {items: 4},
+};
+
+const imgs = [
+    pastorAustraliano,
+    pastorAustraliano,
+    pastorAustraliano,
+    pastorAustraliano,
+    pastorAustraliano,
+    pastorAustraliano,
+    pastorAustraliano,
+    pastorAustraliano,
+    pastorAustraliano,
+    pastorAustraliano,
+    pastorAustraliano]
+
+const items = imgs.map(imgSource=><div className={style.imgContainer}>
+    <img src={imgSource} alt={"pastorAustraliano"} onDragStart={handleDragStart} role="presentation"/>
+</div>)
+
 const Gallery = () => {
     const scrollRef = useHashScroll<HTMLDivElement>("presentation", useHashScrollDefaultOptions);
     return (
         <Section>
             <div className={style.container} ref={scrollRef}>
-                <div className={contentCls}>
-                    <h1 className={fonts.sectionHeader}>This is the title</h1>
-                    <div className={nftContentCls}>
-                        <img src={perrito} alt="husky"/>
-                        <div className={nftDescriptionCls}>
-                            <span>This is some random content about that lovely husky</span>
-                            <span>This is some random content about that lovely husky</span>
-                            <span>This is some random content about that lovely husky</span>
-                            <span>This is some random content about that lovely husky</span>
-                        </div>
-                    </div>
+                <div className={style.content}>
+                    <AliceCarousel
+                        autoPlay
+                        infinite
+                        mouseTracking
+                        touchTracking
+                        animationDuration={2000}
+                        items={items}
+                        responsive={responsive}
+                        controlsStrategy="responsive"
+                        disableButtonsControls
+                    />
                 </div>
             </div>
         </Section>
